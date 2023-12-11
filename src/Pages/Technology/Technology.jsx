ed6c3fe2aed0d7b147dd4bfe459357c3c1cfebdc
alif0013 <mdalifh36@gmail.com>
2023-12-11 21:47:08 +0600

    added design technology

--- src/Pages/Technology/Technology.jsx
+++ src/Pages/Technology/Technology.jsx
@@ -16,29 +16,242 @@ const Technology = () => {
         <div>
             <div data-aos="fade-up" className='mt-10 mb-10 md:mb-20 pt-20'>
                 <h1 className='text-center text-2xl md:text-5xl'>Technology That I Know</h1>
-            {/* <hr className="hidden md:block w-[600px] mx-auto mt-5 border-2 border-purple-600" /> */}
+                {/* <hr className="hidden md:block w-[600px] mx-auto mt-5 border-2 border-purple-600" /> */}
             </div>
 
 
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-                        <div>
-                            
-                            <div data-aos="zoom-in" className="card md:w-60  border-2 border-purple-600 rounded-xl shadow-xl">
-                                <figure className="px-10 pt-10">
-                                    <img  src="https://raw.githubusercontent.com/alif0013/developer-portfolio/e9eb3c43cd153e30cfcd4aa72157c1a49306a46d/src/assets/font-end.svg" alt="img" className="rounded-xl w-[200px] md:h-[100px]" />
-                                </figure>
-                                <div className="card-body items-center text-center">
-                                    <h2 className="card-title">FrontEnd</h2>
-                                    <p>Html, Css, JavaScript, React, Next js</p>
-                                  
+            <div className='grid grid-cols-4 md:grid-cols-8 gap-5'>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/html.webp" />
                                 </div>
                             </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Html</h2>
                         </div>
-
-                        
-                    
                     </div>
-        
+                </div>
+
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/css.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Css</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/javascript.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Jsvascript</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/81e242542b9d65f4f38f03729de6e99d98cbfb81/src/assets/react.svg" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">React</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/node.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Node</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/ex.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Express</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/mongodb.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">MongoDB</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/tailwind.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Tailwind</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/mu5.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Material Ui</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/bootstrap.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Bootstrap</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/firebase.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Firebase</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/git.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Git & Github</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/api.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Api</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/figma.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Figma</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                    <div data-aos="zoom-in" className="card">
+                        <figure className="px-10 pt-10">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/socket-io.webp" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Scoket io</h2>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
         </div>
     );
 };
