c1db57a2a3ef6d14837cf1eda2500ada787f0da3
alif0013 <mdalifh36@gmail.com>
2023-12-12 11:47:37 +0600

    added live site link

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -41,23 +41,37 @@ const Projects = () => {
                 >
                     <SwiperSlide>
                         <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/tapy-co-poster.jpg" />
-                        <Link to='/'><h2>Live Link</h2></Link>
+                        <Link to='https://online-forum-a2790.web.app/' target='_blank'><h2>Live Site</h2></Link>
+                        <Link to='https://github.com/alif0013/online-forum-client' target='_blank'><h2>Code Link</h2></Link>
+                    
                     </SwiperSlide>
                     <SwiperSlide>
                         <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/tryotel-b2c-cover.jpg" />
+                    
+                        <Link to='https://well-off-bit.surge.sh/' target='_blank'><h2>Live Site</h2></Link>
+                        <Link to='https://github.com/alif0013/online-group-study?tab=readme-ov-file' target='_blank'><h2>Code Link</h2></Link>
+                    
+
                     </SwiperSlide>
                     <SwiperSlide>
                         <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/desktop.png" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                        <Link to='/' target='_blank'><h2>Social Media</h2></Link>
+                        <Link to='/' target='_blank'><h2>Comming Soon</h2></Link>
+                    
                     </SwiperSlide>
                     
                     <SwiperSlide>
-                        <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                        <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/swiper01_2.webp" />
+                        <Link to='https://brand-shop-4a376.web.app/' target='_blank'><h2>Live Site</h2></Link>
+                        <Link to='https://github.com/alif0013/brand-shop-client' target='_blank'><h2>Code Link</h2></Link>
+                    
+                    </SwiperSlide>
+                    
+                    <SwiperSlide>
+                        <img src="https://raw.githubusercontent.com/alif0013/developer-portfolio/main/src/assets/swi6.jpg" />
+                        <Link to='https://academist-ea732.web.app/' target='_blank'><h2>Live Site</h2></Link>
+                        <Link to='https://github.com/alif0013/learning-event-management' target='_blank'><h2>Code Link</h2></Link>
+                    
                     </SwiperSlide>
                 </Swiper>
             </div>
